e9cf3709a9cee6b6f15554de6993c9d011e8cec0
James B <jwb50@kent.ac.uk>
2024-01-29 14:00:20 +0000

    update cors policy for development purposes only

--- src/main/java/spring/task/api/controller/TaskController.java
+++ src/main/java/spring/task/api/controller/TaskController.java
@@ -8,7 +8,7 @@ import spring.task.orm.entity.Task;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping(path = "api/v1/task")
 public class TaskController {
